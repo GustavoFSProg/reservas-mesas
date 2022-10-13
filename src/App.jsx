3a9e8b4ad14b31e34076aeeb3dc79ba4e028f53b
Gustavo <gustavo.prog40@gmail.com>
2022-10-13 16:10:45 -0300

    feitom

---
+++ src/App.jsx
@@ -0,0 +1,169 @@
+import mesa from './assets/mesa.jpeg'
+import './App.css'
+
+function App() {
+
+  return (
+    <div className="container">
+      <h1>Sistema de reservas:</h1>
+    <div className="App">
+
+      <div>
+        
+        <img src={mesa} className="logo-mesa" alt="React logo" />
+        <p>
+          RESERVA: 
+          <br />
+          Nome: Arlindo Cruz
+          <br />
+          Telefone: 6969-66696
+
+        </p>
+      </div>
+      <div>
+
+        <img src={mesa} className="logo-mesa" alt="React logo" />
+        <p>
+          RESERVA:
+          <br />
+          Nome: Maria Prado
+          <br />
+          Telefone: 6969-66696
+
+        </p>
+      </div>
+      <div>
+
+        <img src={mesa} className="logo-mesa" alt="React logo" />
+
+        <p>
+          RESERVA:
+          <br />
+          Nome: Paula Tejjanno
+          <br />
+          Telefone: 6969-66696
+
+        </p>
+      </div>
+      <div>
+
+        <img src={mesa} className="logo-mesa" alt="React logo" />
+
+        <p>
+          RESERVA:
+          <br />
+          Nome: Pedro Souza
+          <br />
+          Telefone: 6969-66696
+
+        </p>
+      </div>
+      <div>
+
+        <img src={mesa} className="logo-mesa" alt="React logo" />
+
+        <p>
+          RESERVA:
+          <br />
+          Nome: Pedro Souza
+          <br />
+          Telefone: 6969-66696
+
+        </p>
+      </div>
+     
+      <div>
+
+        <img src={mesa} className="logo-mesa" alt="React logo" />
+        <p>
+          RESERVA:
+          <br />
+          Nome: Pedro Souza
+          <br />
+          Telefone: 6969-66696
+
+        </p>
+      </div>
+      <div>
+
+        <img src={mesa} className="logo-mesa" alt="React logo" />
+        <p>
+          RESERVA:
+          <br />
+          Nome: Pedro Souza
+          <br />
+          Telefone: 6969-66696
+
+        </p>
+      </div>
+      <div>
+
+        <img src={mesa} className="logo-mesa" alt="React logo" />
+        <p>
+          RESERVA:
+          <br />
+          Nome: Pedro Souza
+          <br />
+          Telefone: 6969-66696
+
+        </p>
+      </div>
+
+      <div>
+
+        <img src={mesa} className="logo-mesa" alt="React logo" />
+        <p>
+          RESERVA:
+          <br />
+          Nome: Pedro Souza
+          <br />
+          Telefone: 6969-66696
+
+        </p>
+      </div>
+
+      <div>
+
+        <img src={mesa} className="logo-mesa" alt="React logo" />
+        <p>
+          RESERVA:
+          <br />
+          Nome: Pedro Souza
+          <br />
+          Telefone: 6969-66696
+
+        </p>
+      </div>
+      <div>
+
+        <img src={mesa} className="logo-mesa" alt="React logo" />
+        <p>
+          RESERVA:
+          <br />
+          Nome: Pedro Souza
+          <br />
+          Telefone: 6969-66696
+
+        </p>
+      </div>
+      <div>
+
+        <img src={mesa} className="logo-mesa" alt="React logo" />
+        <p>
+          RESERVA:
+          <br />
+          Nome: Pedro Souza
+          <br />
+          Telefone: 6969-66696
+
+        </p>
+      </div>
+
+
+      </div>
+    </div>
+
+  )
+}
+
+export default App
